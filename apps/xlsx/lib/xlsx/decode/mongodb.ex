84d72712d37fe076592e1f0530d5d978139cdd04
Leticia Hernández <leticia.hernandez@hovahealth.com>
2021-02-09 21:49:27 -0600

    Warnings Leticia Hernández[dev.hevl@medical-minds.net]

--- apps/xlsx/lib/xlsx/decode/mongodb.ex
+++ apps/xlsx/lib/xlsx/decode/mongodb.ex
@@ -15,18 +15,18 @@ defmodule Xlsx.Decode.Mongodb do
 
 
 
-  defp priv_decode({map, acc}, []) do
+  defp priv_decode({_map, acc}, []) do
     acc
   end
   defp priv_decode({map, acc}, [key|t]) do
     value = priv_decode({map, %{}}, key, map[key])
     priv_decode({map, Map.put(acc, key, value) }, t)
   end
-  defp priv_decode({map, acc}, "_id", value) when is_struct(value) do
+  defp priv_decode({map, _acc}, "_id", value) when is_struct(value) do
     {:ok, id} = BSON.ObjectId.encode(map["_id"])
     id
   end
-  defp priv_decode({map, acc}, key, value) when is_map(value) do
+  defp priv_decode({_map, _acc}, _key, value) when is_map(value) do
     new_value = case Map.keys(value) do
       [:__struct__, :calendar, :day, :hour, :microsecond, :minute, :month, :second, :std_offset, :time_zone, :utc_offset, :year, :zone_abbr] ->
         value
@@ -34,18 +34,18 @@ defmodule Xlsx.Decode.Mongodb do
     end
     new_value
   end
-  defp priv_decode({map, acc}, key, []) do
+  defp priv_decode({_map, _acc}, _key, []) do
     []
   end
-  defp priv_decode({map, acc}, key, value) when is_list(value) do
-    [h|t] = value
+  defp priv_decode({_map, _acc}, _key, value) when is_list(value) do
+    [h|_t] = value
     case is_map(h) do
       :true -> decode(value)
       _-> value
     end
   end
 
-  defp priv_decode({map, acc}, key, value) do
+  defp priv_decode({_map, _acc}, _key, value) do
     value
   end
 end
