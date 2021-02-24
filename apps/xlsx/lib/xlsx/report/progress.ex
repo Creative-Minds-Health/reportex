86c5c32e2b4eacb0a351c4751530fe1d3ba6c1c5
Enrique Guerrero <dev.gume@medical-minds.net>
2021-02-24 11:04:42 -0600

    Issue en funcion para mostrar el progreso en el master

--- apps/xlsx/lib/xlsx/report/progress.ex
+++ apps/xlsx/lib/xlsx/report/progress.ex
@@ -68,7 +68,7 @@ defmodule Xlsx.Report.Progress do
     end
     {:ok, date} = DateTime.now("America/Mexico_City")
     {:ok, response} = Poison.encode(Map.put(map, "total", total) |> Map.put("status", "doing") |> Map.put("socket_id", socket_id) |> Map.put("date_last_update", format_date(date)))
-    LibLogger.send(res_socket, response)
+    LibLogger.send_progress(res_socket, response)
     #:gen_tcp.send(res_socket, response)
     {:noreply, state, progress_timeout}
   end
