8dd8002d2d289038efc5b8cd397aacea1241cd07
Leticia Hernandez <dev.hevl@medical-minds.net>
2021-04-13 13:26:29 -0500

    datos [Leticia Hernandez Villanueva - dev.hevl@medical-minds.net]

--- apps/xlsx/lib/xlsx/srs_web/consult/consult.ex
+++ apps/xlsx/lib/xlsx/srs_web/consult/consult.ex
@@ -19,40 +19,45 @@ defmodule Xlsx.SrsWeb.Consult.Consult do
   def get_value(item, [], _field, _default_value) do
     item
   end
-  def get_value(item, [_h|_t], "consultation_date", _default_value) do
-    case Map.get(item, "consultation_date", :undefined) do
-      :undefined -> ""
-      date ->
-        date
-        # Calendar.strftime(DateTime.shift_zone!(date, "America/Mexico_City"), "%Y-%m-%d")
-    end
-  end
-  def get_value(item, [_h|_t], "patient|splited_age|years", _default_value) do
-    case Map.get(item, "patient", %{}) |> Map.get("splited_age", %{}) |> Map.get("years", :undefined) do
-      :undefined -> ""
-      age -> Integer.to_string(trunc(age)) <> " AÑOS"
-    end
-  end
-  def get_value(item, [_h|_t], "patient|dh", _default_value) do
+
+  def get_value(item, [_h|_t], "affiliate_program", default_value) do
     case Map.get(item, "patient", %{}) |> Map.get("dh", :undefined)do
-      :undefined -> ""
+      :undefined -> default_value
       dh -> get_dh(dh)
     end
   end
-  def get_value(item, [_h|_t], "clue", _default_value) do
-    Map.get(item, "clue", "") <> " - " <> Map.get(item, "clue_name", "")
+  def get_value(item, [_h|_t], "patient|migrant|key", default_value) do
+    case Map.get(item, "patient", %{}) |> Map.get("migrant", %{}) |> Map.get("key", :undefined) do
+      1 -> "X"
+      _ -> default_value
+    end
   end
-  def get_value(item, [_h|_t], "reference_data", _default_value) do
-    hospital_key_clues = Map.get(item, "reference_data", %{}) |> Map.get("hospital_key_clues", "")
-    hospital_name = Map.get(item, "reference_data", %{}) |> Map.get("hospital_name", "")
-    hospital_key_clues <> " - " <> hospital_name
+  def get_value(item, [_h|_t], "patient|indigenous|key", default_value) do
+    case Map.get(item, "patient", %{}) |> Map.get("indigenous", %{}) |> Map.get("key", :undefined) do
+      1 -> "X"
+      _ -> default_value
+    end
   end
-  def get_value(item, [_h|_t], "diagnosis", _default_value) do
-    diagnosis_key = Map.get(item, "reference_data", %{}) |> Map.get("diagnosis", %{}) |> Map.get("key", "")
-    diagnosis_description = Map.get(item, "reference_data", %{}) |> Map.get("diagnosis", %{}) |> Map.get("description", "")
-    diagnosis_key <> " - " <> diagnosis_description
+  def get_value(item, [_h|_t], "patient|disability", default_value) do
+    case Map.get(item, "patient", %{}) |> Map.get("disability", :undefined) do
+      true -> "X"
+      _ -> default_value
+    end
   end
+  def get_value(item, [_h|_t], "diagnosis", default_value) do
+    diagnosis = Map.get(item, "diagnosis", []);
+    new_diagnosis = case length(diagnosis) do
+      0 ->
+        [%{}, %{}, %{}]
+      1 ->
+        diagnosis ++ [%{}, %{}]
+      2 ->
+        diagnosis ++ [%{}]
+      _ -> diagnosis
 
+    end
+    {:multi, get_diagnosis(new_diagnosis, [], 0)}
+  end
   def get_value(item, [h|t], field, default_value) do
     case Map.get(item, h, :undefined) do
       :undefined -> default_value
@@ -64,17 +69,35 @@ defmodule Xlsx.SrsWeb.Consult.Consult do
     end
   end
 
-  def sinba_date(date) do
-    DateLib.string_date(date, "-")
+  def get_diagnosis(diagnosis, list, 3) do
+    list
+  end
+  def get_diagnosis([h|t], list, count_diagnosis) do
+    new_list = case Map.get(h, "first_time", :undefined) do
+      0 -> list ++ ["Subsecuente"]
+      1 -> list ++ ["1ra vez"]
+      _ -> list ++ [""]
+    end
+
+    new_desc = case {Map.get(h, "key_diagnosis", :undefined), Map.get(h, "description", :undefined)} do
+      {:undefined, :undefined} -> new_list ++ [""]
+      {key, description} ->
+        new_list ++ [key <> " - " <> description]
+    end
+
+    get_diagnosis(t, new_desc, count_diagnosis + 1)
   end
 
   def get_dh(dh_list) when length(dh_list) <= 0 do
     ""
   end
   def get_dh(dh_list) when length(dh_list) >= 1 do
-    case for item <- dh_list, item["key"] === 1 and item["affiliate_program"] === 1, do: item do
+    case for item <- dh_list, item["key"] === 1, do: item do
       [principal_entitled | _] ->
-        Map.get(principal_entitled, "insurance_policy", "")
+        case Map.get(principal_entitled, "affiliate_program", :undefined) do
+          1 -> "X"
+          _ -> ""
+        end
       _->
         ""
     end
